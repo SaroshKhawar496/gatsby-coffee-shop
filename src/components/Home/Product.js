9480c2adb5147ce9acfa6c485573e49730f43d7f
SaroshKhawar496 <sarosh.496@gmail.com>
2019-12-06 14:10:21 -0800

    Integrated snipcart

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -9,8 +9,16 @@ const Product = ({ product }) => {
         <div className="card-body text-center">
           <h6 className="text-capitalize">{product.title}</h6>
           <h6>${product.price}</h6>
-          <button className="btn btn-yellow mt-3 text-capitalize">
-            add to card
+          <button
+            className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
+            data-item-id={product.id}
+            data-item-price={product.price}
+            data-item-name={product.title}
+            // need to use image src with snipcart
+            data-item-image={product.image.fluid.src}
+            data-item-url="https://gatsby-regular-joes.netlify.com/"
+          >
+            add to cart
           </button>
         </div>
       </div>
